1aa1fd5b4f8282594c41bf8e01801be789deaed3
tart2000 <tart2000design@gmail.com>
2015-04-12 11:33:17 -0400

    better mobile gifs

--- space_gif_online/site/snippets/gifs.php
+++ space_gif_online/site/snippets/gifs.php
@@ -2,7 +2,7 @@
   <div class="row">
 
     <?php foreach (page('gifs')->images() as $image) : ?>
-      <div class="col-md-3">
+      <div class="col-md-3 col-sm-4 col-xs-6">
         <a href="<?php echo $image->url() ?>">
           <img src="<?php echo $image->url() ?>" alt="" class="img-responsive impb">
         </a>
