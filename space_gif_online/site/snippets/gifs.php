b9f7e715dbe0431656cf9019dd659bbb5b8c04c7
tart2000 <tart2000design@gmail.com>
2015-04-12 11:31:44 -0400

    blind css

--- space_gif_online/site/snippets/gifs.php
+++ space_gif_online/site/snippets/gifs.php
@@ -4,7 +4,7 @@
     <?php foreach (page('gifs')->images() as $image) : ?>
       <div class="col-md-3">
         <a href="<?php echo $image->url() ?>">
-          <img src="<?php echo $image->url() ?>" alt="" class="img-responsive">
+          <img src="<?php echo $image->url() ?>" alt="" class="img-responsive impb">
         </a>
       </div>
     <?php endforeach ?>
