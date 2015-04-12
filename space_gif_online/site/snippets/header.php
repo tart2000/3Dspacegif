fd5189d096098b3eeb929653d5822670eec9fab4
tart2000 <tart2000design@gmail.com>
2015-04-12 10:46:54 -0400

    all dependencies added

--- space_gif_online/site/snippets/header.php
+++ space_gif_online/site/snippets/header.php
@@ -8,10 +8,9 @@
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <script src="assets/js/jquery-2.1.3.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
   <link rel="icon" type="image/ico" href="/assets/images/favicon.ico" />
 
   <?php echo css('assets/css/main.css') ?>
