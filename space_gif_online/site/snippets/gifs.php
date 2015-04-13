e39e33ce91f1351f6aa713c289f80514236f6d12
tart2000 <tart2000design@gmail.com>
2015-04-13 10:53:35 -0400

    towards pagination

--- space_gif_online/site/snippets/gifs.php
+++ space_gif_online/site/snippets/gifs.php
@@ -1,13 +1,17 @@
 
   <div class="row">
+    <?php $gifpage = page('gifs')->images()->flip()->paginate(12) ?>
 
-    <?php foreach (page('gifs')->images()->flip() as $image) : ?>
+    <?php foreach ($gifpage as $image) : ?>
       <div class="col-md-3 col-sm-4 col-xs-6">
         <a href="<?php echo $image->url() ?>">
           <img src="<?php echo $image->url() ?>" alt="" class="img-responsive impb">
         </a>
       </div>
     <?php endforeach ?>
+
+
+
 
   </div>
 
